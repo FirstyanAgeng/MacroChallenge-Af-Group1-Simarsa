6d8cd102237c7e964d4144ae6d43b403534c2d02
atmoo-wil <wilaga.atmo@gmail.com>
2022-11-24 12:10:21 +0700

    menambah animasi di daftarBerhasil.jsx dan memperbaiki kartuAntrian.jsx

--- src/pages/admin/KartuAntrian.jsx
+++ src/pages/admin/KartuAntrian.jsx
@@ -1,43 +1,108 @@
-import { Container, Card, Row } from "react-bootstrap";
+import { Container, Card, Row, Col } from "react-bootstrap";
 import "../../styles/admin.css";
+import { useNavigate } from "react-router-dom";
+import { ADMIN_DASHBOARD} from "../../router";
+
 
 const KartuAntrian = () =>{
+  const navigate = useNavigate();
     return(
         <div className="kartuAntrian pageAntrian">
             <Container className="cont9 text-center">
-            <h3 className="t9 bold">
+            <p className="t9 bold">
                 KARTU ANTRIAN
-            </h3>
-            <Card className="cardAntrian card mt-5">
+            </p>
+            <Card className="cardAntrian card">
             <Card.Title className="cardTA">
                 <h4>No Antrian</h4>
             </Card.Title>
                 
-            <Card.Body>
-                <Row className="content9">
-                <h2 className="mt-5 mb-4" style={{color:"black"}}>
+            <Card.Body style={{marginTop:"0px"}}>
+                <Row className="content9 mb-3">
+                <p className="mt-2 mb-2 idl">
                 <strong>UM01</strong>
                 <span>
-                  <h3 style={{color:"black"}}>
+                  <p className="idl1">
                     Kamis
-                  </h3>
+                  </p>
                 </span>
-              </h2>
+              </p>
                 </Row>
                 <Row className="content9">
-                <h2 className="mt-5 mb-4" style={{color:"black"}}>
-                <strong>Dokter</strong>
+                <p className="mt-1 mb-1 idl2">
+                Dokter
                 <span>
-                  <h3 style={{color:"black"}}>
+                  <p className="idl3">
                 dr. Wahyu Mustiadi, Sp. P. M.Kes 
-                  </h3>
+                  </p>
                 </span>
-              </h2>
+              </p>
                 </Row>
 
+                <Row>
+                  <Col sm={6}l className="content8">
+                  <p className="mt-3 idl2">
+                Poli Tujuan
+                <span>
+                  <p className="idl4">
+                POLI PARU
+                  </p>
+                </span>
+              </p>
+                  </Col>
+
+                  <Col sm={6}l className="content8">
+                  <p className="mt-3 idl2">
+                Nama
+                <span>
+                  <p className="idl4">
+                Tengku Mahmudi
+                  </p>
+                </span>
+              </p>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col sm={6}l className="content8">
+                  <p className="mt-3 idl2">
+                No. rekam Medis
+                <span>
+                  <p className="idl4">
+                  PS.123458888
+                  </p>
+                </span>
+              </p>
+                  </Col>
+
+                  <Col sm={6}l className="content8">
+                  <p className="mt-3 idl2">
+                Waktu
+                <span>
+                  <p className="idl4">
+                  08:00-11:00
+                  </p>
+                </span>
+              </p>
+                  </Col>
+                </Row>
             </Card.Body>
             </Card>
+
+            <Row className="text-center mt-2">
+              <Col sm={6}>
+                <button className="btn btn9" onClick={() => navigate(ADMIN_DASHBOARD)}
+                >Home</button>
+              </Col>
+              <Col sm={6}>
+                <button className="btn btn7">Print</button>
+              </Col>
+
+            </Row>
             </Container>
+
+
+            
         </div>
     );
 };
